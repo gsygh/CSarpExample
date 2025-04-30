73ccfc57ab11d520f1390e1e6e37c79ab08a6b8a
gsygh <gsygh@naver.com>
2025-04-30 21:48:30 +0900

    example

--- C#Basic/Program.cs
+++ C#Basic/Program.cs
@@ -61,14 +61,6 @@
  *               select new { user.Id, user.Name };
  */
 
-// 상수
-//const int SUNDAY = 0;
-//const int MONDAY = 1;
-//const int TUESDAY = 2;
-//const int WEDNESDAY = 3;
-//const int THURSDAY = 4;
-//const int FRIDAY = 5;
-//const int SATURDAY = 6;
 
 
 // 비교 연산자
@@ -93,10 +85,75 @@
 // int? b = null;   int a = b ?? 3; >> b의 값이 null인 경우, 3을 반환
 // int? a = 2;  int? b = 5;     a ??= b; >> a의 값이 null인 경우, b의 값을 할당
 
+
+
+// Switch 문 표현식(C# 8.0 이상)
+/* string grade = "B+";
+string message = grade switch
+{
+    "A" => "우수한 성적입니다.",
+    string g when g == "B" || g == "B+" => "좋은 성적입니다.",
+    "C" => "보통 성적입니다.",
+    _ => "예외 상황"
+}; */
+
+
+
+
+// 배열 접근
+/* string[] fruits = ["사과", "바나나", "레몬"];
+Console.WriteLine(fruits[^2]);  --> 기존의 [fruits.Length - 2] 와 같음 */
+
+
+
+// 다차원 배열
+//int[,] matrix = { { 1, 2, 3 }, { 4, 5, 6 } };
+/* Console.WriteLine(matrix[1, 1]);
+Console.WriteLine(matrix.GetLength(0)); => 1차원의 개수 
+
+2차원 배열의 foreach
+foreach(int[] array in arrays) {
+    foreach (int item in array) {
+        Console.Write(item + " ");
+    }
+    Console.Writeline();
+}
+ */
+
+
+
+// Array 클래스 메서드
+/*
+Array.Sort(array) : 배열을 오름차순으로 정렬
+Array.Reverse(array) : 배열의 순서를 반대로 뒤집기
+Array.indexOf(array, value) : 배열에서 특정 값의 인덱스를 찾음
+Array.Resize(ref array, size) : 배열의 크기를 조정함 
+ */
+
+
+// 반복문
+/* 
+foreach (string fruit in fruits) { Console.WriteLine(fruit); } => 컬렉션의 모든 요소를 순회할 때 사용
+
+do { Console.WriteLine($"카운트: {count}"); count++; } while (count < 3);  => 실행 후 조건문 검사 => 첫 한 번은 무조건 실행
+ */
+
+
+
 // 최상위문
 // 최상위문은 네임스페이스 및 형식 선언 앞에 와야 함
 // 시작점인 main 함수를 늘 선언하는 것은 불필요 > 해당 내용을 클래스를 생략하고 앞에 선언함으로서 최상위문을 선언 
 
+
+
+// 상수
+//const int SUNDAY = 0;
+//const int MONDAY = 1;
+//const int TUESDAY = 2;
+//const int WEDNESDAY = 3;
+//const int THURSDAY = 4;
+//const int FRIDAY = 5;
+//const int SATURDAY = 6;
 enum Days       // '상수'들의 집합 (함수 내 정의 불가)
 {
     Sunday,             // 0
@@ -117,6 +174,12 @@ public class Program
     {
         Days days = Days.Monday;
         Console.WriteLine("main : " + days);
+        Console.Write("숫자를 입력하세요 : ");
+        String? input = Console.ReadLine();
+        Console.WriteLine("입력한 숫자는 " + input + " 입니다.");
+
+        int number = int.Parse(input ?? "0");
+        Console.WriteLine("입력한 숫자는 " + number + " 입니다.");
         // 콘솔 프로그램에서 키 입력을 기다릴 때 사용하는 메소드
         Console.ReadKey();
     }
